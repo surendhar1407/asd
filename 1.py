592a52a4eec2c8116e4d4ce8f7a1046da78fc8b6
surendhar1407 <surendharmkns@gmail.com>
2023-05-16 12:31:22 +0530

    as soon

--- 1.py
+++ 1.py
@@ -3,9 +3,3 @@ b = 27
 print(a,b)
 
 
-def pypart(n):     
-    for i in range(0, n):
-        for j in range(0, i+1):
-
-            print("* ",end="")
-        print("\r")
---
+++ 123.py
@@ -0,0 +1,6 @@
+
+a = 1811
+b = 2227
+print(a+b)
+print(a-b)
+print(a*b)
